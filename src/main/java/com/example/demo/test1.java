00da9e9103c83dc5b7b0df28d9d9e8ab867a40d1
依鹏 <824834815@qq.com>
2022-06-11 20:08:13 +0800

    yipeng0611

---
+++ src/main/java/com/example/demo/test1.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+public class test1 {
+    public void testmethod(){
+        System.out.println(System.currentTimeMillis());
+    }
+
+    public static void main(String[] args) {
+        System.out.println(System.currentTimeMillis());
+    }
+}
